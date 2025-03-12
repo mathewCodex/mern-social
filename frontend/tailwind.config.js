d53ca54a9df9b06fb34b3bc25fb11469862e079c
mathewCodex <mathewemmanuel212@gmail.com>
2025-03-12 05:05:46 +0100

    mern stack app completed fixed socket.io and notification sounds

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,20 @@
+// /** @type {import('tailwindcss').Config} */
+// export default {
+// 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+// 	theme: {
+// 		extend: {},
+// 	},
+// 	// eslint-disable-next-line no-undef
+// 	plugins: [require("daisyui")],
+// };
+
 /** @type {import('tailwindcss').Config} */
+import daisyui from "daisyui"; // Changed from require to import
+
 export default {
-	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-	theme: {
-		extend: {},
-	},
-	// eslint-disable-next-line no-undef
-	plugins: [require("daisyui")],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [daisyui], // Use the imported variable
 };
